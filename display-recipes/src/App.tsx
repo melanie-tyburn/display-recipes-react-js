0feb65e8ffde047032abe1705f2d3bc182adf2d9
Melanie-Tyburn <tyburn.melanie@gmail.com>
2023-11-28 10:43:20 +0100

    Update App.tsx

--- display-recipes/src/App.tsx
+++ display-recipes/src/App.tsx
@@ -9,10 +9,7 @@ const App = () => {
   return (
     <>
       <Header
-        title={recipe.title}
-        author={recipe.author}
-        difficulty={recipe.difficulty}
-        thumbnail={recipe.thumbnail}
+        {...recipe}
       />
        <Ingredients ingredients={recipe.ingredients} />
        <Instructions instructions={recipe.instructions} />
